0363e6a69bf1c17b4e9670ec69e8074017d2df96
Ruthlolo <ifeoluwa.ruth.olonijolu@umontreal.ca>
2025-05-14 11:04:24 -0400

    Initial commit

---
+++ practise.R
@@ -0,0 +1,4 @@
+## Using a similar function of input and print in python in R
+f_n <- as.integer(readline(prompt = "Enter your favourite number: "))
+print(paste("Favourite number is:", f_n))
+## in R, print() does not take multiple arguments
